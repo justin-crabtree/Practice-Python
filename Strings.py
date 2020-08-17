1ff33782a055f59bb38921727b0e3df23784a168
Justin Crabtree <54284826+justin-crabtree@users.noreply.github.com>
2020-08-17 15:29:13 -0400

    Create Strings.py
    adds strings.py file

---
+++ Strings.py
@@ -0,0 +1,9 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " world")
+
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + " " + name)
