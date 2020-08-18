1ee852261906f14f619e3531b21384d0b8550816
Justin Crabtree <54284826+justin-crabtree@users.noreply.github.com>
2020-08-18 13:00:35 -0400

    adds order of operations to file

--- Operators.py
+++ Operators.py
@@ -5,10 +5,23 @@ print(a + b)
 print(a - b)
 print(a * b)
 print(a / b)
-print(a // b)
-print(a % b)
+print(a // b)   # integer division, rounded down towards minus infinity
+print(a % b)    # modulo: the remainer after integer division
 
 print()
 
-for i in range(1, 4):
-  print(i)
+# for i in range(1, a // b):
+  # print(i) 
+
+print(a + b / 3 - 4 * 12)
+print (a + (b/3) - (4 * 12))
+print ((((a + b) / 3) - 4) * 12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print (e * 12)
+
+print ()
+
+print (a / (b * a) / b)
