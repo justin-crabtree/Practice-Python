4a0b61325ba7e8aa43825cc18b770eb1c4e8efe5
Justin Crabtree <54284826+justin-crabtree@users.noreply.github.com>
2020-08-18 17:03:06 -0400

    Create SliceBack.py

---
+++ SliceBack.py
@@ -0,0 +1,15 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[25:0:-1] # doesnt include 'a'
+backwards2 = letters[::-1] 
+
+print(backwards2)
+
+# create a slice that = qpo
+print(letters[16:13:-1])
+
+# create a slice that = edcba
+print(letters[4::-1])
+
+# create a slice that = last 8 characters in reverse
+print(letters[25:17:-1])
