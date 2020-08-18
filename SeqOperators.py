ad2f1e639921a6120b8375450f0b89ef4c587551
Justin Crabtree <54284826+justin-crabtree@users.noreply.github.com>
2020-08-18 17:29:32 -0400

    Create SeqOperators.py

---
+++ SeqOperators.py
@@ -0,0 +1,18 @@
+string = "There "
+string2 = "was "
+string3 = "a "
+string4 = "hole here, "
+string5 = "now it's "
+string6 = "gone"
+
+print(string + string2 + string3 + string4 + string5 + string6)
+
+print('Yo ' * 3)
+print('Yo ' * (5 + 4))
+print('Yo ' * 5 + "4")
+
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("thur" in today)
+print("parrot" in "gone")
