4e3f2ac6c4bf5c0db80690bc4903465b1a51411b
Justin Crabtree <54284826+justin-crabtree@users.noreply.github.com>
2020-08-19 13:24:20 -0400

    adds voting block example

--- Blocks.py
+++ Blocks.py
@@ -1,3 +1,13 @@
-for i in range(1, 13):
-  print("The number {} squared is {} and cubed is {:4}".format(i, i ** 2, i **3))
-print("*" * 80)
+# for i in range(1, 13):
+#   print("The number {} squared is {} and cubed is {:4}".format(i, i ** 2, i **3))
+# print("*" * 80)
+
+name = input("Please enter your name: ")
+age = int(input("How old are you, {0}? ".format(name)))
+#print(age)
+
+if age >= 18:
+  print("You're old enough to vote!")
+  print("Please put an X in the box")
+else:
+  print("Please come back in {0} years to vote!".format(18 - age))
