b962d7de3dcc75f335d92dba6934ff0b53edfe8d
Justin Crabtree <54284826+justin-crabtree@users.noreply.github.com>
2020-08-19 13:45:34 -0400

    adds yoda logic to file

--- Blocks.py
+++ Blocks.py
@@ -6,8 +6,18 @@ name = input("Please enter your name: ")
 age = int(input("How old are you, {0}? ".format(name)))
 #print(age)
 
-if age >= 18:
+# if age >= 18:
+#   print("You're old enough to vote!")
+#   print("Please put an X in the box")
+# elif age == 900:
+#   print("Hello Yoda!")
+# else:
+#   print("Please come back in {0} years to vote!".format(18 - age))
+
+if age < 18:
+  print("Please come back in {0} years to vote!".format(18 - age))
+elif age == 900:
+  print("Hello Yoda!")
+else:
   print("You're old enough to vote!")
   print("Please put an X in the box")
-else:
-  print("Please come back in {0} years to vote!".format(18 - age))
