2adce285eb10fde433efebaeea66bc160566e563
Justin Crabtree <54284826+justin-crabtree@users.noreply.github.com>
2020-08-20 13:53:25 -0400

    reformatted code, added != syntax

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,21 +1,36 @@
 answer = 5
 
 print("Please guess a number between 1 and 10: ")
-guess = int(input())
+guess = int(input())         
 
-if guess < answer:
-  print("One more try, guess higher")
+if guess != answer:
+  if guess < answer:
+    print("One more try, guess higher")
+  else:
+    print("One more try, guess lower")
   guess = int(input())
   if guess == answer:
-    print("Well done, you guessed it!")
+    print("Correct!")
   else:
-    print("Sorry, you guessed incorrectly")
-elif guess > answer:
-  print("One more try, guess lower")
-  guess = int(input())
-  if guess == answer:
-    print("Well done, you guessed it!")
-  else:
-    print("Sorry, you guessed incorrectly")
+    print("Incorrect!")
 else:
-  print("Correct!")
+  print("Beginners luck...")
+
+
+# if guess < answer:    
+#   print("One more try, guess higher")
+#   guess = int(input())
+#   if guess == answer:
+#     print("Well done, you guessed it!")
+#   else:
+#     print("Sorry, you guessed incorrectly") 
+# elif guess > answer:
+#   print("One more try, guess lower")
+#   guess = int(input())
+#   if guess == answer:
+#     print("Well done, you guessed it!")   
+#   else:
+#     print("Sorry, you guessed incorrectly")
+# else:
+#   print("Correct!")
+
