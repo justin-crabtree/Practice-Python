27e0685ee702db103e5f3391c05c3a888e8a580f
Justin Crabtree <54284826+justin-crabtree@users.noreply.github.com>
2020-08-21 20:37:56 -0400

    adds or conditional

--- Conditions.py
+++ Conditions.py
@@ -1,4 +1,8 @@
 age = int(input("How old are you? "))
 
-if age >= 16 and age <= 65:
+#if age >= 16 and age <= 65:
+
+if 16 <= age <= 65:
   print("Have a good day at work")
+else:
+  print("Enjoy your free time")
