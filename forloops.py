14ab8081123f0aff3d9562e49e05085e872af4f9
Justin Crabtree <54284826+justin-crabtree@users.noreply.github.com>
2020-08-25 16:54:56 -0400

    adds number loop example

--- forloops.py
+++ forloops.py
@@ -1,4 +1,16 @@
-parrot = "Norwegian Blue"
+# parrot = "Norwegian Blue"
 
-for character in parrot:
-  print(character)
+# for character in parrot:
+#   print(character)
+
+number = "9,223;372:036 854,775;807"
+seperators = ""
+
+for char in number:
+  if not char.isnumeric():
+    seperators = seperators + char
+
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
